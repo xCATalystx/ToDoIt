b16b172e82f8ab476744ef4eeac170081695fff6
fangyu <susie.salmon.1973.12.06@gmail.com>
2021-07-19 11:59:11 +0800

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,6 @@ Rails.application.routes.draw do
     get "sign_up", to: "users#new"
     post "sign_up", to: "users#create"
 
-    get "sign_in", to: "sessions#new"
-    post "sign_in", to: "sessions#create"
-
-    delete "logout", to: "sessions#destroy"
-
-    get 'password/reset', to: 'password_resets#new'
-    post 'password/reset', to: 'password_resets#create'
-    get 'password/reset/edit', to: 'password_resets#edit'
-    patch 'password/reset/edit', to: 'password_resets#update'
-
     root "users#index"
   end
 
--- db/migrate/20210626074757_create_tasks.rb
+++ db/migrate/20210626074757_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.due_date :date
+      t.date :due_date
       t.string :priority
       t.string :status
       t.string :tag
