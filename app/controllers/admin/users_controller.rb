b950e0ccb1ca43657745abbf8269e3aa94294e8f
fangyu <susie.salmon.1973.12.06@gmail.com>
2021-07-26 09:40:11 +0800

    edit admin/users_controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :find_user, only:[:edit, :show, :update, :destroy]
   
   def index
-    @users = User.all
+    @users = User.all.order(id: :desc)
   end
 
   def new
